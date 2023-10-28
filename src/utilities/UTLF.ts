78955f33da41b227433002bc01b8de13d43e23dd
Max-Menke <max.menke.123@gmail.com>
2023-10-28 09:40:32 +0200

    utilities : [ # Change name to abbreviation # adding new funktions : ctiveClass , combineRefs , slideTo ]

---
+++ src/utilities/UTLF.ts
@@ -0,0 +1,11 @@
+// UTLF : Utility Funktion
+
+import { slideTo }  from './ts/slideTo.ts';
+import  combineRefs from './ts/combineRefs.ts';
+import { activeClass } from './ts/activeClass.ts';
+
+export  {
+  slideTo,
+  combineRefs,
+  activeClass,
+}
