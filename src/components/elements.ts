cd7caf8528f1f3798008f727581227091f264cc3
Max-Menke <max.menke.123@gmail.com>
2023-11-02 09:37:14 +0100

    elements : [ #adding all all new elements ]

--- src/components/elements.ts
+++ src/components/elements.ts
@@ -1,10 +1,14 @@
 //Atoms
   import ExtendedDiv from './atoms/ExtendedDiv/ExtendedDiv';
   import DottSliderDisplay from './atoms/DottSliderDisplay/DottSliderDisplay';
+  import ColorChangeSVG from './atoms/ColorChangeSVG/ColorChangeSVG';
 
 // Molecules
   import HorizontalSlider from './molecules/HorizontalSlider/HorizontalSlider';
   import TimelineElement from './molecules/TimelineElement/TimelineElement';
+  import IconShowcase from './molecules/IconShowcase/IconShowcase';
+  import EndlessSliderVertical from './molecules/EndlessSliderVertical/EndlessSliderVertical';
+  import GridHorizontalSlider from './molecules/GridHorizontalSlider/GridHorizontalSlider';
 
 // Templates
   import MainWindowFrame from './templates/MainWindowFrame/MainWindowFrame';
@@ -13,15 +17,20 @@
 // Organisms
   import ProfileInfo from './organisms/ProfileInfo/ProfileInfo';
   import FullSizeTimeline from './organisms/FullSizeTimeline/FullSizeTimeline';
+  import SkillsInfo from './organisms/SkillsInfo/SkillsInfo';
 
 export  {
 //Atoms
   ExtendedDiv,
   DottSliderDisplay,
+  ColorChangeSVG,
 
 // Molecules
   HorizontalSlider,
   TimelineElement,
+  IconShowcase,
+  EndlessSliderVertical,
+  GridHorizontalSlider,
 
 // Templates
   MainWindowFrame,
@@ -30,4 +39,5 @@ export  {
 // Organisms
   ProfileInfo,
   FullSizeTimeline,
+  SkillsInfo,
 }
